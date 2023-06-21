a266f4136888b09212bc1257e4df0e5c118a488f
lilmortyj <781113402@qq.com>
2023-06-21 03:38:06 +0000

    add plot heatmap

---
+++ draw/plot_heatmap.py
@@ -0,0 +1,58 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+plt.style.use("seaborn-whitegrid")
+
+def load_file(file_path, key_words):
+    # load csv files using pandas
+    df = pd.read_csv(file_path)
+    data = np.array(df[key_words])  # (num_data, 2)
+    return data
+
+def plot_heatmap(data, name='result'):
+    X_range = (-4, 20)
+    Y_range = (-4, 20)
+    X_range_p = (-4, 20)
+    Y_range_p = (-6, 6)
+    print(f'num_data: {len(data)}')
+    fig = plt.figure(figsize=(10, 6))
+    ax = fig.add_subplot(1, 1, 1)
+    ax.set_title(name.split('/')[-1])
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    if "states" in name:
+        if "AntMazeT" in name:
+            for i in range(X_range_p[0], X_range_p[1]+1):
+                ax.vlines(i, Y_range_p[0], Y_range_p[1], colors="k")  # x=?
+            for j in range(Y_range_p[0], Y_range_p[1]+1):
+                ax.hlines(j, X_range_p[0], X_range_p[1], colors="k")  # y=?
+        elif "AntMaze" in name:
+            for i in range(X_range[0], X_range[1]+1):
+                ax.vlines(i, Y_range[0], Y_range[1], colors="k")  # x=?
+            for j in range(Y_range[0], Y_range[1]+1):
+                ax.hlines(j, X_range[0], X_range[1], colors="k")  # y=?
+        ax.scatter(data[:,0], data[:,1], s=1, alpha=0.2)
+    elif "goals" in name:
+        ax.scatter(data[:,0], data[:,1], s=2, alpha=0.4)
+    plt.savefig(name + '.png', dpi=400, bbox_inches='tight')
+
+if __name__ == '__main__':
+    root_dir = '../pics/'
+    if not os.path.exists(root_dir):
+        os.makedirs(root_dir)
+    file_lists = [
+        '../results/AntMazeT-v0_hiro_2_traindata.csv',
+    ]
+    key_words = [
+        ['h_state_x',
+        'h_state_y',],
+    ]
+    name_list = [
+        '_h_states',
+    ]
+    for k in range(len(file_lists)):
+        name_ = file_lists[k].split('/')[-1].split('.')[0]
+        name = root_dir + name_ + name_list[k]
+        data = load_file(file_lists[k], key_words[k])
+        plot_heatmap(data, name=name)
